45053077fc63e1a65387d831728df9fc2564fd83
Victor Borja <vborja@apache.org>
2015-01-11 02:44:12 -0600

    Use PORT

--- example.nim
+++ example.nim
@@ -1,5 +1,10 @@
 # example.nim
-import jester, asyncdispatch, htmlgen
+import jester, asyncdispatch, htmlgen, os, strutils
+
+var settings = newSettings()
+
+if existsEnv("PORT"):
+  settings.port = Port(parseInt(getEnv("PORT")))
 
 routes:
   get "/":
